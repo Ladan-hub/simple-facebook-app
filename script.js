342a05a335690c96e6afd20ead64c42ce7587b0f
Ladan Nazari <ladannazary@gmail.com>
2023-05-23 11:31:54 -0700

    works but is buggy

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 var database = [
-    {
-        username: "ladan",
-        password: "7777777"
-    },
-    {
-        username: "sally",
-        password: "123"
-    },
-    {
-        username: "yasaman",
-        password: "789"
-    },
-    {
-        username: "bardia",
-        password: "hahaha"
-    },
-
-]
-
+  {
+    username: "ladan",
+    password: "7777777",
+  },
+  {
+    username: "sally",
+    password: "123",
+  },
+  {
+    username: "yasaman",
+    password: "789",
+  },
+  {
+    username: "bardia",
+    password: "hahaha",
+  },
+];
 
 var timeline = [
-    {
-        username: "Peter",
-        timeline: "I love being an engineer!"
-    },
-    {
-        username: "Armaghan",
-        timeline: "I love living in Miami!"
-    }
-]
+  {
+    username: "Peter",
+    timeline: "I love being an engineer!",
+  },
+  {
+    username: "Armaghan",
+    timeline: "I love living in Miami!",
+  },
+];
 
-var usernamePrompt = prompt("What's your username?")
-var passwordPrompt = prompt("What's your password?")
+var usernamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
 
-function signIn (username, password) {
+function signIn(username, password) {
+//   database.map((user) => {
+//     if (username === user.username && password === user.password) {
+//       return timeline;
+//     } else {
+//       alert("Sorry, wrong username and password!");
+//     }
+//   });
 
-
-    if (username === database[0].username && password === database[0].password) {
-        return timeline;
-    } else {
-        alert("Sorry, wrong username and password!")
+    for (var i = 0; i < database.length; i++) {
+        if (username === database[i].username && password === database[i].password) {
+            console.log(timeline)
+        } else {
+            alert("Sorry, wrong username and password")
+        }
     }
 }
 
-
 signIn(usernamePrompt, passwordPrompt);
-
-
-
-
